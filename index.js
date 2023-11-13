f5c38df039851a8a7970538cd7e20b1771ef83b6
Jessie Chen <112643063+cheny311@users.noreply.github.com>
2023-11-12 21:49:04 -0500

    index.js

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ import express from "express";
 
 //create an instance of an express application
 const app = express();
-app.use(express.json())
+app.use(express.json());
 //set the port the application will run on
 const port = process.env.PORT || 3001;
 
-
-//set up response for the root path of the application
-app.get("/", (req, res) => {
+// //set up response for the root path of the application
+app.get("/firstName/:firstName/lastName/:lastName", (req, res) => {
   // res.send("Draw a cow");
+  console.log(req.params);
   res.json({
-    data: "response hello"
-  })
+    data: `The full name is ${req.params.firstName} ${req.params.lastName}`,
+  });
+  res.json({
+    data: "response hello",
+  });
+});
+
+app.get("/", (req, res) => {
+  console.log("test");
 });
 
 //set up the application to listen on the specified port
